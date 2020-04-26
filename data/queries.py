8560d1269a7641c2a70d947dabc1fce3c4b29eb1
Kovacs Katt <kattkovacs@gmail.com>
2020-04-26 12:47:46 +0200

    sql query

--- data/queries.py
+++ data/queries.py
@@ -3,3 +3,19 @@ from data import data_manager
 
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
+
+def get_genre_tops(genre):
+    return data_manager.execute_select(
+        """SELECT shows.title,
+       shows.year,
+       shows.rating
+
+FROM shows
+Join show_genres sg ON 
+shows.id = sg.shows.id
+JOIN genres g ON sg.genre_id = g.id
+WHERE g.name = %{genre}s 
+ORDER BY shows.rating DESC
+LIMIT 10;""",
+        {"genre": genre}
+    )
