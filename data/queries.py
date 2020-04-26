f8641cbbec375f9cc5422fc0392af18728a1db8e
Kovacs Katt <kattkovacs@gmail.com>
2020-04-26 14:15:26 +0200

    sql ok

--- data/queries.py
+++ data/queries.py
@@ -4,4 +4,12 @@ from data import data_manager
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
 
-# in progress
+def get_years():
+    return data_manager.execute_select(
+        """
+        SELECT date_part('year', year) as year, AVG(rating) as avarage, COUNT(id)
+        FROM shows
+        WHERE date_part('year', year) >= 1970 and date_part('year', year) <= 2010
+        GROUP BY year;
+        """
+    )
