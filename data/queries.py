372e62d8f08d48f05d121aa40c7a5e33ea47c68d
Kovacs Katt <kattkovacs@gmail.com>
2020-04-27 10:47:34 +0200

    htmls, queries, main.py written js need

--- data/queries.py
+++ data/queries.py
@@ -3,3 +3,13 @@ from data import data_manager
 
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
+
+def get_show_by_title(search):
+    return data_manager.execute_select(
+        """
+        SELECT *
+        FROM shows
+        WHERE LOWER(title) LIKE LOWER(%(search)s)
+        ORDER BY id;
+        """, {'search': search}
+    )
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from data import queries
 
 app = Flask('codecool_series')
@@ -6,13 +6,16 @@ app = Flask('codecool_series')
 
 @app.route('/')
 def index():
-    shows = queries.get_shows()
-    return render_template('index.html', shows=shows)
+    return render_template('search.html')
 
 
-@app.route('/design')
+@app.route('/shows_by_name', methods=["POST", "GET"])
 def design():
-    return render_template('design.html')
+    if request.method == "POST":
+        original_search = request.form['search']
+        search = '%' + original_search + '%'
+        shows = queries.get_show_by_title(search)
+    return render_template('index.html', shows=shows, search=original_search)
 
 
 def main():
