d360337f1e80ff54f23c90459296a02cae8eb42e
Kovacs Katt <kattkovacs@gmail.com>
2020-04-30 19:32:01 +0200

    JS !write out avg

--- static/js/main.js
+++ static/js/main.js
@@ -6,23 +6,43 @@ for (let card of cards) {
 
 
 function changeBackground(e) {
-        let selectedCard = e.target.parentElement;
+        let selectedCard = this;
         if (selectedCard.classList.contains('highlighted')) {
             selectedCard.classList.remove('highlighted');
         } else {
-            selectedCard.classList.add('highlighted');
             let collectHighs = document.querySelectorAll('.highlighted');
-            if (collectHighs.length===1){
+            selectedCard.classList.add('highlighted');
+            getAverage();
 
-                let oneHighChild = document.querySelector('.avg');
-                document.querySelector('#avg').innerHTML = oneHighChild.text;
-            } else {
-                for (let high of collectHighs) {
-                    let highChild = document.querySelector('.avg');
-                    document.querySelector('#avg').innerHTML = highChild.text;
-                }
-            }
+            // if (collectHighs.length===1){
+            //
+            //     let oneHighChild = document.querySelectorAll('.avg');
+            //     document.querySelector('#avg').innerHTML = oneHighChild.text;
+            // } else {
+            //     for (let high of collectHighs) {
+            //         let highChild = document.querySelector('.avg');
+            //         document.querySelector('#avg').innerHTML = highChild.text;
+            //     }
+            // }
         }
+
+function getAverage() {
+    let highlighteds = document.querySelectorAll('.highlighted');
+    let counter = 1;
+    if (highlighteds.length===1) {
+        let average = selectedCard.querySelector('h1').textContent;
+        document.querySelector('#avg').innerHTML = average;
+
+    } else {
+        let newAverage = selectedCard.querySelector('h1').textContent;
+        counter++;
+        let oldAvg = document.querySelector('#avg').innerHTML;
+        let newTotal = (parseFloat(newAverage)+parseFloat(oldAvg))/counter;
+        document.querySelector('#avg').innerHTML = String(newTotal);
+    }
+
+}
+
 }
 
 
