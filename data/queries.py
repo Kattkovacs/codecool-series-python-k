ee9f3fa17888e94554cb8c7f36e1005975daae3c
Kovacs Katt <kattkovacs@gmail.com>
2020-04-30 08:02:03 +0200

    js not yet error at selection

--- data/queries.py
+++ data/queries.py
@@ -4,14 +4,15 @@ from data import data_manager
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
 
+
 def get_selected_shows():
     return data_manager.execute_select(
         """
-        SELECT shows.title, COUNT(e.id)
+        SELECT shows.title, shows.overview, COUNT(e.id)
 FROM shows
          JOIN seasons s on shows.id = s.show_id
          JOIN episodes e on s.id = e.season_id
-GROUP BY shows.title
+GROUP BY shows.title, shows.overview
 HAVING COUNT(e.id) < ((
                           SELECT COUNT(e.id)
                           FROM episodes as e
--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,12 @@
-showCards=document.querySelectorAll('.card-shows');
+showCards=document.querySelectorAll('.card');
 
 for (let card of showCards) {
     card.addEventListener('click', function () {
-
-
+        let showOverview=document.querySelectorAll('.overview');
+        if (showOverview.style.display === 'none') {
+            showOverview.style.display = 'block';
+        } else {
+            showOverview.style.display = 'none';
+        }
     })
 }
