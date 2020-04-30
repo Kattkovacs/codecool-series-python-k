661dff548a3ce51c269430114c2c4e3f1d55e6ce
Kovacs Katt <kattkovacs@gmail.com>
2020-04-30 15:28:25 +0200

    JS !write out avg

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,31 @@
-cards = document.querySelectorAll('.header').parentElement;
-
+let cards = document.querySelectorAll('.card-show');
 
 for (let card of cards) {
-    card.addEventListener('click', changeBackground)
+    card.addEventListener('click', changeBackground);
 }
 
+
 function changeBackground(e) {
-    let selectedCard = e.target.parentElement;
-    selectedCard.classList.add('highlighted');
+        let selectedCard = e.target.parentElement;
+        if (selectedCard.classList.contains('highlighted')) {
+            selectedCard.classList.remove('highlighted');
+        } else {
+            selectedCard.classList.add('highlighted');
+            let collectHighs = document.querySelectorAll('.highlighted');
+            if (collectHighs.length===1){
+
+                let oneHighChild = document.querySelector('.avg');
+                document.querySelector('#avg').innerHTML = oneHighChild.text;
+            } else {
+                for (let high of collectHighs) {
+                    let highChild = document.querySelector('.avg');
+                    document.querySelector('#avg').innerHTML = highChild.text;
+                }
+            }
+        }
 }
+
+
+
+//     .children;
+// document.querySelector('#avg').innerHTML = collectHighs;
