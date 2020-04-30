28a9ef400d2e78dc244bacf17b0961c965c7c960
Kovacs Katt <kattkovacs@gmail.com>
2020-04-30 13:26:47 +0200

    finished

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,9 @@
-showCards=document.querySelectorAll('.card');
+let showCards=document.querySelectorAll('.card-shows');
 
 for (let card of showCards) {
     card.addEventListener('click', function () {
-        let showOverview=document.querySelectorAll('.overview');
-        if (showOverview.style.display === 'none') {
+        let showOverview=card.querySelector('.overview');
+        if (showOverview.style.display === 'none' || showOverview.style.display === '') {
             showOverview.style.display = 'block';
         } else {
             showOverview.style.display = 'none';
