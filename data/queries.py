482a861bf735ea0a537e9814c97a3590f8310551
Kovacs Katt <kattkovacs@gmail.com>
2020-05-01 16:28:10 +0200

    'ImmutableMultiDict' object is not callable main.py

--- data/queries.py
+++ data/queries.py
@@ -2,5 +2,22 @@ from data import data_manager
 
 
 def get_shows():
-    return data_manager.execute_select('SELECT id, title FROM shows;')
+    return data_manager.execute_select(
+        """
+        SELECT id, title, date_part('year', CAST(shows.year AS DATE)) as year FROM shows;
+        """)
 
+
+def get_shows_by_given_year(year):
+    return data_manager.execute_select(
+        """
+        SELECT a.name, (date_part('year', CAST(shows.year AS DATE)) - date_part('year', a.birthday)) as age, 
+        to_char(date_part('year', shows.year)) as year
+        FROM shows
+        JOIN show_characters sc on shows.id = sc.show_id
+        JOIN actors a on sc.actor_id = a.id
+        GROUP BY a.name, age, shows.year
+        HAVING year = %(year)s
+        ORDER BY age ASC;
+        """, {'year': year}
+    )
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from data import queries
 
 app = Flask('codecool_series')
 
 
-@app.route('/')
+@app.route('/', methods=["POST", "GET"])
 def index():
+    if request.method == "POST":
+        year = request.post('year')
+        shows = queries.get_shows_by_given_year(year)
+        print(shows)
+        return render_template('index.html', shows=shows, year=year)
     shows = queries.get_shows()
+    print(shows)
     return render_template('index.html', shows=shows)
 
 
