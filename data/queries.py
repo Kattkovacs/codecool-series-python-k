bbff6d4f6f7ddbe7c186bf220f53851c76451915
Kovacs Katt <kattkovacs@gmail.com>
2020-05-05 17:48:33 +0200

    works sql make case part to select perfect(can't) js need

--- data/queries.py
+++ data/queries.py
@@ -8,15 +8,21 @@ def get_shows():
 def get_query_by_selection(selected_type, order):
     return data_manager.execute_select(
         """
-        SELECT shows.title as shows, s.title as seasons, e.title as episodes
-        FROM shows
-        JOIN seasons s on shows.id = s.show_id
-        JOIN episodes e on s.id = e.season_id
-        GROUP BY shows.title, s.title, e.title
-        ORDER BY 
-        CASE WHEN %(order)s = 'ASC' THEN %(selected_type)s END ASC,
-        CASE WHEN %(order)s = 'DESC' THEN %(selected_type)s END DESC
+        SELECT
+    shows.title as shows, s.title as seasons, e.title as episodes
+FROM shows
+         JOIN seasons s on shows.id = s.show_id
+         JOIN episodes e on s.id = e.season_id
+GROUP BY shows.title, s.title, e.title
+ORDER BY
+            CASE WHEN %(selected_type)s ='shows' AND %(order)s = 'ASC' THEN shows.title END,
+        CASE WHEN %(selected_type)s ='shows' AND %(order)s = 'DESC' THEN shows.title END DESC,
+        CASE WHEN %(selected_type)s ='seasons' AND %(order)s = 'ASC' THEN  s.title END,
+        CASE WHEN %(selected_type)s ='seasons' AND %(order)s = 'DESC' THEN shows.title END DESC,
+        CASE WHEN %(selected_type)s ='episodes' AND %(order)s = 'ASC' THEN  e.title END,
+        CASE WHEN %(selected_type)s ='episodes' AND %(order)s = 'DESC' THEN shows.title END DESC
         LIMIT 50;
+
         """, {'selected_type': selected_type, 'order': order}
 
     )
