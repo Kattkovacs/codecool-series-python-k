b8dd189e5a592429a2258471efebc270829de910
Kovacs Katt <kattkovacs@gmail.com>
2020-05-06 18:08:38 +0200

    first part finished (modification sql not working)

--- data/queries.py
+++ data/queries.py
@@ -8,11 +8,13 @@ def get_shows():
 def get_min_episode_season(min_episode, min_seas):
     return data_manager.execute_select(
         """
-        SELECT shows.id, shows.title, s.season_number, e.episode_number
+        SELECT shows.title, s.season_number, e.episode_number, g.name
         FROM shows
         JOIN seasons s on shows.id = s.show_id
         JOIN episodes e on s.id = e.season_id
-        GROUP BY shows.id, s.season_number, e.episode_number
+        JOIN show_genres sg on shows.id = sg.show_id
+        JOIN genres g on sg.genre_id = g.id
+        GROUP BY g.name, shows.title, s.season_number, e.episode_number
         HAVING MIN(s.season_number) >= %(min_seas)s AND MIN(e.episode_number) >= %(min_episode)s;
         """, {'min_episode': min_episode, 'min_seas': min_seas}
     )
