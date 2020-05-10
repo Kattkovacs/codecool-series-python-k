0c0b33c004d4451f82b5d442257ea883285ada38
Kovacs Katt <kattkovacs@gmail.com>
2020-05-10 18:12:50 +0200

    finished

--- data/queries.py
+++ data/queries.py
@@ -3,3 +3,16 @@ from data import data_manager
 
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
+
+
+def get_genres_rating():
+    return data_manager.execute_select(
+        """
+                SELECT genres.name, AVG(ROUND(s.rating, 2))
+        FROM genres
+                 JOIN show_genres sg on genres.id = sg.genre_id
+                 JOIN shows s on sg.show_id = s.id
+        GROUP BY genres.name, s.rating
+        ORDER BY AVG(ROUND(s.rating, 2));
+        """
+    )
