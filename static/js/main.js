2cfe535a9fa75cb2f8988eace386eccc1a5616f2
Kovacs Katt <kattkovacs@gmail.com>
2020-05-10 12:02:36 +0200

    finished

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,33 @@
-let rating = document.querySelectorAll('.fas fa-star');
+let stars = document.querySelectorAll('i');
 
+for (let star of stars) {
+    star.addEventListener('mouseenter', starHoverHandler);
+}
+for (let star of stars) {
+    star.addEventListener('mouseleave', starLeaveHandler);
+}
 
+function starHoverHandler(e) {
+    let starId = e.target.dataset.starId;
+    let row = e.target.parentNode;
+    let rowStars = row.querySelectorAll('i');
+    for (let star of rowStars) {
+        if (star.dataset.starId <= starId) {
+            star.className = "fas fa-star";
+        } else {
+            star.className = "far fa-star";
+        }
+    }
+}
 
-for (let rate of rating) {
-    rate.addEventListener('mouseenter', ()=>{
-
-    });
+function starLeaveHandler(e) {
+    let row = e.target.parentNode;
+    let rowStars = row.querySelectorAll('i');
+    for (let star of rowStars) {
+        if(row.dataset.showRating-1 >= star.dataset.starId) {
+            star.className = "fas fa-star";
+        } else {
+            star.className = "far fa-star";
+        }
+    }
 }
