19f9d6d0938f40f2071335315632be67f5c57f8b
Kovacs Katt <kattkovacs@gmail.com>
2020-05-11 11:53:39 +0200

    finished

--- data/queries.py
+++ data/queries.py
@@ -2,4 +2,11 @@ from data import data_manager
 
 
 def get_shows():
-    return data_manager.execute_select('SELECT id, title FROM shows;')
+    return data_manager.execute_select(
+        """
+        SELECT actors.name
+        FROM actors
+        WHERE LOWER(LEFT(actors.name, 1)) = 'a' OR actors.name LIKE '% A%'
+        ORDER BY actors.name DESC;
+"""
+    )
